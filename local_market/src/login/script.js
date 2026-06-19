1c9a69a4499a6fe83d475b8b87bb548a293cef6c
Júlio <tdsjsa2010@gmail.com>
2026-06-19 11:06:19 -0300

    Delete local_market directory

--- local_market/src/login/script.js
+++
@@ -1,9 +0,0 @@
-function login(){
-    var nome = $("#nome").val()
-    var senha = $("#senha").val()
-    if(nome && senha && nome === "admin" && senha === "12345"){
-
-    }else{
-        
-    }
-}
