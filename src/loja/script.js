93e9bac25d878664960122a76bca34e60c6a9ea0
Júlio <tdsjsa2010@gmail.com>
2026-06-23 12:39:37 -0300

    Add files via upload

--- src/loja/script.js
+++ src/loja/script.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded",function(){
             produtos.forEach((produto, index) =>{
                 const card = document.createElement("div")
                 card.innerHTML =
-                                    `<div class="card" style="width: 18rem;">
+                    `<div class="card" style="width: 18rem;">
                     <img src="${produto.imagem}" class="card-img-top" alt="${produto.desc}">
                     <div class="card-body">
                         <h5 class="card-title">${produto.desc}</h5>
